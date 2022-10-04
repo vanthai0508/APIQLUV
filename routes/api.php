6612428525df283e8687769626a3ba311168eb34
vanthai0508 <93816353+vanthai0508@users.noreply.github.com>
2022-10-04 20:08:25 +0700

    reset

--- routes/api.php
+++ routes/api.php
@@ -51,11 +51,11 @@ Route::group([ 'prefix' => 'auth' ], function ()
             Route::group(['middleware' => 'User-Account'], function()
             {
                 
-                Route::get('letterconfirm', 'App\Http\Controllers\ConfirmController@letterConfirm');
+                // Route::get('letterconfirm', 'App\Http\Controllers\ConfirmController@letterConfirm');
 
-                Route::post('userconfirm', 'App\Http\Controllers\ConfirmController@userConfirm');
+                // Route::post('userconfirm', 'App\Http\Controllers\ConfirmController@userConfirm');
 
-                Route::post('create', 'App\Http\Controllers\CVController@create');
+                // Route::post('create', 'App\Http\Controllers\CVController@create');
             
             });
 
@@ -68,27 +68,27 @@ Route::group([ 'prefix' => 'auth' ], function ()
             Route::group(['middleware' => 'User-Account-Admin'], function()
             {
 
-                Route::get('list', 'App\Http\Controllers\CVController@list');
+                // Route::get('list', 'App\Http\Controllers\CVController@list');
 
-                Route::get('find/{id}', 'App\Http\Controllers\CVController@find');
+                // Route::get('find/{id}', 'App\Http\Controllers\CVController@find');
 
-                Route::post('done/{id}', 'App\Http\Controllers\CVController@done');
+                // Route::post('done/{id}', 'App\Http\Controllers\CVController@done');
 
-                Route::get('getuser/{id}', 'App\Http\Controllers\CVController@getUser');
+                // Route::get('getuser/{id}', 'App\Http\Controllers\CVController@getUser');
 
-                Route::get('reject/{id}', 'App\Http\Controllers\CVController@reject');
+                // Route::get('reject/{id}', 'App\Http\Controllers\CVController@reject');
 
-                Route::post('createconfirm', 'App\Http\Controllers\ConfirmController@create');
+                // Route::post('createconfirm', 'App\Http\Controllers\ConfirmController@create');
 
-                Route::get('listconfirm', 'App\Http\Controllers\ConfirmController@list');
+                // Route::get('listconfirm', 'App\Http\Controllers\ConfirmController@list');
 
-                Route::get('check', 'App\Http\Controllers\UserController@check');
+                // Route::get('check', 'App\Http\Controllers\UserController@check');
 
-                Route::get('valueconfirm/{id}', 'App\Http\Controllers\CVController@valueConfirm');
+                // Route::get('valueconfirm/{id}', 'App\Http\Controllers\CVController@valueConfirm');
 
-                Route::post('approve/{id}', 'App\Http\Controllers\CVController@approve');
+                // Route::post('approve/{id}', 'App\Http\Controllers\CVController@approve');
 
-                Route::get('listuserconfirmed', 'App\Http\Controllers\ConfirmController@listUserParticipationInterview');
+                // Route::get('listuserconfirmed', 'App\Http\Controllers\ConfirmController@listUserParticipationInterview');
             
 
 
